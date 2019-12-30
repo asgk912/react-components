fbbff52690c0edb85f96f68be5ec9ad87a6f077b
Albert <asgk912@gmail.com>
2019-12-30 13:27:52 -0800

    implement different components, one of which is a class, to handle hover events

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-// TODO
+var GroceryList = (props) => (
+	<ul>		
+		{props.groceryItems.map(item => <GrocerListItem item={item}/>)}		
+	</ul>
+);
+
+
+//<GroceryListItem item = {props.groceryItems[0]} />
+// <GroceryListItem item = {props.groceryItems[1]} />
+
+/* Creating and rendering nested React components
+var Rice = () => (
+	<li>Rice</li>
+)
+
+var Pasta = () => (
+	<li>Psta</li>
+)
+*/
+
+/* Component Properties aka "props"
+var GroceryListItem = (props) => (
+	<li>{props.item}</li>
+)
+*/
+
+class GrocerListItem extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+      		hovered: false
+		};
+	}
+
+	boldOnMouseOver() {
+    	this.setState({
+      		hovered: true
+    	});
+	}
+
+	unboldOnMouseOut() {
+    	this.setState({
+      		hovered: false
+    	});
+	}
+
+	render() {
+		var style = {
+      		fontWeight: this.state.hovered ? 'bold' : 'normal'
+    	};
+
+		return (
+			<li style={style} onMouseOver={this.boldOnMouseOver.bind(this)} onMouseOut={this.unboldOnMouseOut.bind(this)}>{this.props.item}</li>
+		);
+	}
+}
+
+var groceryItems = ['Rice', 'Pasta'];
+
+ReactDOM.render(<GroceryList groceryItems = {groceryItems}/>, document.getElementById("app"));
